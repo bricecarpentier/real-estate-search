cdb9b8a7d9451c1b7acea80231d22bd14fc527d6
Brice Carpentier <brice@daknet.org>
2010-10-29 20:26:30 +0200

    - added a downloader that :   - is able to queue many urls to download   - emits events when one download finishes as well as when there is nothing more to download  - updated run.js

---
+++ downloader.js
@@ -0,0 +1,74 @@
+var sys     = require('sys'),
+    events  = require('events'),
+    request = require('request');
+
+const STEP  = 'STEP',
+      ERROR = 'ERROR',
+      ALL   = 'ALL';
+
+var Downloader = function() {
+    events.EventEmitter.call(this);
+    
+    this.urls = [];
+}
+
+sys.inherits(Downloader, events.EventEmitter);
+
+var onNextDownloadCallback = function() {
+    if (this.urls.length == 0) {
+        this.emit(ALL);
+    }
+}
+
+var onDownloadCallback = function(url, error, response, body) {
+    if (!error && response.statusCode == 200) {
+        this.emit(STEP, {url: url, data: body});
+    } else {
+        this.emit(ERROR, {url: url});
+    }
+}
+
+Downloader.prototype.setURLs = function(urls) {
+    this.urls = urls;
+    
+    return this;
+}
+
+Downloader.prototype.getURLs = function() {
+    return this.urls;
+}
+
+Downloader.prototype.downloadURL = function(url) {
+    var self = this;
+    request({uri:url}, function() {
+        var args = [url];
+        for (var i=0 ; i<arguments.length ; i++) {
+            args.push(arguments[i]);
+        }
+        onDownloadCallback.apply(self, args);
+    })
+    return this;
+}
+
+Downloader.prototype.downloadNext = function() {
+    if (this.urls.length) {
+        this.once(STEP, function() {onNextDownloadCallback.apply(this)});
+        return this.downloadURL(this.urls.pop());
+    }
+}
+
+Downloader.prototype.download = function() {
+    var self = this;
+    var boundDownloadNext = function() {
+        self.downloadNext();
+    }
+    this.on(STEP, boundDownloadNext);
+    this.once(ALL, function() {self.removeListener(STEP, boundDownloadNext)});
+    return this.downloadNext();
+}
+
+
+exports.STEP = STEP;
+exports.ALL  = ALL;
+
+exports.Downloader = Downloader;
--- run.js
+++ run.js
@@ -1,14 +1,13 @@
-var sys            = require('util'),
-    request        = require('request'),
-    ejs            = require('ejs'),
-    fs             = require('fs'),
-    parser_factory = require('./parser-factory');
+var sys             = require('sys'),
+    request         = require('request'),
+    ejs             = require('ejs'),
+    fs              = require('fs'),
+    downloader      = require('./downloader'),
+    parser_factory  = require('./parser-factory');
 
 var url = 'http://www.pap.fr/annonce/vente-appartement-loft-atelier-maison-lyon-1er-g35369g35370g35371g35372g35374g35375g35376g35377';
 
 var template = fs.readFileSync('./template.js', 'utf8');
-
-var parser = parser_factory.build(url);
 
 var render = function(items) {
     sys.puts(ejs.render(template, {
@@ -18,12 +17,9 @@ var render = function(items) {
     }));
 }
 
-request({uri:url}, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var items = parser.parse(body, function(items) {
-//            console.log(items)
-            render(items)
-        });
-    }
-});
-
+var d = new downloader.Downloader();
+d.on(downloader.STEP, function(downloadedData) {
+    var parser = parser_factory.build(downloadedData.url);
+    parser.parse(downloadedData.data, render);
+})
+d.downloadURL(url);
