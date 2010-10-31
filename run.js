227876385dbe96d9a7fe5b3d07422f9b25c1aab1
Brice Carpentier <brice@daknet.org>
2010-10-31 18:49:05 +0100

    updated code to use the express framework.
    As of this commit, the app is expected to be called as a webapp

--- run.js
+++ run.js
@@ -3,49 +3,62 @@ var sys             = require('util'),
     ejs             = require('ejs'),
     fs              = require('fs'),
     downloader      = require('./downloader'),
-    parser          = require('./parser');
+    parser          = require('./parser'),
+    express         = require('express');
 
-var url = 'http://www.pap.fr/annonce/vente-appartement-loft-atelier-maison-lyon-1er-g35369g35370g35371g35372g35374g35375g35376g35377';
 
-var template = fs.readFileSync('./template.js', 'utf8');
-
-var render = function(items) {
-    items.sort(function(a,b) {
-        return a.created_on - b.created_on;
-    })
+var slash = function(req, res) {
+    var url = 'http://www.pap.fr/annonce/vente-appartement-loft-atelier-maison-lyon-1er-g35369g35370g35371g35372g35374g35375g35376g35377';
     
-    sys.puts(ejs.render(template, {
-        locals: {
-            items:items
+    var render = function(items) {
+        items.sort(function(a,b) {
+            return a.created_on - b.created_on;
+        })
+
+        res.render('template.ejs', {
+            locals: {
+                items:items
+            },
+            layout: false,
+            headers: {'Content-Type': 'application/rss+xml',},
+        });
+    }
+
+    var nb = 1;
+    var liste = [];
+
+    var store = function(items) {
+        nb--;
+
+        for (var i=0 ; i<items.length ; i++) {
+            liste.push(items[i]);
         }
-    }));
+
+        if (nb == 0) {
+            render(liste);
+        }
+    }
+
+
+    var d = new downloader.Downloader();
+    d.setURLs([url]);
+    d.on(downloader.STEP, function(downloadedData) {
+        var p = parser.factory.build(downloadedData.url);
+        p.once(parser.END, store)
+        p.parse(downloadedData.data);
+    })
+    d.on(downloader.ERROR, function() {
+        nb--;
+        sys.puts('fail');
+    })
+    d.download();
 }
 
-var nb = 1;
-var liste = [];
-
-var store = function(items) {
-    nb--;
-    
-    for (var i=0 ; i<items.length ; i++) {
-        liste.push(items[i]);
-    }
-    
-    if (nb == 0) {
-        render(liste);
-    }
-}
 
 
-var d = new downloader.Downloader();
-d.setURLs([url]);
-d.on(downloader.STEP, function(downloadedData) {
-    var p = parser.factory.build(downloadedData.url);
-    p.once(parser.END, store)
-    p.parse(downloadedData.data);
-})
-d.on(downloader.ERROR, function() {
-    nb--;
-    sys.puts('fail');
-})
-d.download();
+
+var app = express.createServer();
+
+app.get('/feed.rss', slash);
+
+app.listen(3000);
