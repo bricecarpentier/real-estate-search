162af4dbc174bfd57cd0b3be3187f2fcc6c13d5a
Brice Carpentier <brice@daknet.org>
2010-11-11 12:01:01 +0100

    - changed the way the db connection is handled : one now needs to call feed_model_factory to get a FeedModel instance  - updated run.js to be coherent with generic-crud updates  - replaced more custom controllers by generic ones

--- models.js
+++ models.js
@@ -1,5 +1,9 @@
 var mongoose = require('mongoose').Mongoose;
 
+var db_connection_factory = function() {
+    return mongoose.connect('mongodb://localhost/test')
+}
+
 mongoose.model('Feed', {
     properties: [
         'name',
@@ -20,16 +24,53 @@ mongoose.model('Feed', {
     getters: {
         id: function() {
             return this._id.toHexString();
+        },
+        absolute_url: function() {
+            return '/feeds/' + this.id
+        },
+        edit_url: function() {
+            return this.absolute_url + '/edit'
         }
     },
     
-    static: {
-        findAll: function(callback) {
-            return this.find().all(callback)
-        }
-    }
+    methods: {
+        updateWithFormData: function(data) {
+            for (var prop in data)
+                this[prop] = data[prop]
+        },
+    },
 })
 
-var db = mongoose.connect('mongodb://localhost/test');
+var feed_model_factory = function(db) {
+    return db.model('Feed')
+}
 
-exports.Feed = db.model('Feed');
+var FeedPeer = function() {}
+FeedPeer.prototype.createFromFormData = function(data, callback, db) {
+    db = db || db_connection_factory()
+    var model = feed_model_factory(db)
+    var object = new model()
+    object.updateWithFormData(data)
+    object.save()
+    callback(object)
+}
+FeedPeer.prototype.updateObjectFromFormData = function(object, data, callback) {
+    object.updateWithFormData(data)
+    object.save()
+    callback(object)
+}
+FeedPeer.prototype.findAll = function(callback, db) {
+    db = db || db_connection_factory()
+    
+    var model = feed_model_factory(db)
+    model.find().all(callback)
+}
+FeedPeer.prototype.findById = function(id, callback, db) {
+    db = db || db_connection_factory()
+    
+    var model = feed_model_factory(db)
+    model.findById(id, callback)
+}
+
+exports.feed_peer = new FeedPeer()
+exports.feed_model_factory = feed_model_factory
--- run.js
+++ run.js
@@ -11,18 +11,22 @@ app.use(express.cookieDecoder());
 app.use(express.session());
 app.use(express.methodOverride());
 
-app.get('/feeds/', generics.object_list(models.Feed.findAll.bind(models.Feed), 'index.ejs'));
+app.get('/feeds/', generics.object_list(models.feed_peer.findAll, 'index.ejs'));
 
-app.post('/feeds/', feed.post_edit_feed);
+app.post('/feeds/', generics.object_create(models.feed_peer.createFromFormData, forms.edit));
 app.get('/feeds/new', generics.direct_to_template('feed/edit.ejs', {
     form: forms.edit,
     object: null
 }));
 
 app.get('/feeds/:id.rss', feed.display);
-app.put('/feeds/:id', feed.post_edit_feed);
-app.delete('/feeds/:id', generics.object_delete(models.Feed, '/feeds/'));
-app.get('/feeds/:id/edit', generics.object_detail(models.Feed, 'feed/edit.ejs', {form: forms.edit}));
-app.get('/feeds/:id/delete', generics.object_detail(models.Feed, 'feed/delete.ejs'))
+app.put('/feeds/:id', generics.object_update(models.feed_peer.findById,
+                                             models.feed_peer.updateObjectFromFormData,
+                                             forms.edit));
+app.delete('/feeds/:id', generics.object_delete(models.feed_peer.findById, '/feeds/'));
+app.get('/feeds/:id/edit', generics.object_detail(models.feed_peer.findById,
+                                                  'feed/edit.ejs',
+                                                  {form: forms.edit}));
+app.get('/feeds/:id/delete', generics.object_detail(models.feed_peer.findById, 'feed/delete.ejs'))
 
 app.listen(3000);
